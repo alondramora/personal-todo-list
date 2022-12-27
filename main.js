a8274267ddaa9cb0886f051eb47491a9f4bb059b
alondramora <mora_ac@yahoo.com>
2022-12-26 17:12:52 -0800

    added delete function

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", (e) => {
     console.log("button clicked");
 
     formValidation();
-})
+});
 
 let formValidation = () => {
     if (input.value === "") { // if the input value is empty
@@ -17,5 +17,37 @@ let formValidation = () => {
     } else {    // if the input value is not empty
         console.log("success")
         msg.innerHTML = "";
+        acceptData();
     }
- };
+};
+ 
+// store data from input fields in an object called data
+
+let data = {};
+
+//we will collect data using this function and store it in our data object above
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data);
+
+    createPost();
+};
+
+// C: create
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+        <p>${data.text}</p>
+        <span class="options">
+            <i onClick="editPost(this)" class="fas fa-edit"></i>
+            <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+        </span>
+    </div>
+    `;
+    input.value = "";
+}
+
+// D: delete
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+};
