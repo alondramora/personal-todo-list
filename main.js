87f95ef08152fadff0e1979e0c2047effe22156a
alondramora <mora_ac@yahoo.com>
2023-01-04 20:44:51 -0800

    added in js functions to being to add tasks to the task list

---
+++ main.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector('#new-task-form');
+    const input = document.querySelector('#new-task-input');
+    const list_el = document.querySelector('#tasks')
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault(); // stops the form from refreshing the page since forms refresh pages by default
+        
+        const task = input.value;
+
+        if (!task) { // if the input has no value/ no task
+            alert('Please add a task.')
+            return;
+        }
+        const task_el = document.createElement('div');
+        task_el.classList.add('task');
+
+        const task_content_el = document.createElement('div');
+        task_content_el.classList.add('content');
+        task_content_el.innerText = task;
+
+        task_el.appendChild(task_content_el);
+
+        list_el.appendChild(task_el);
+    
+    })
+})
